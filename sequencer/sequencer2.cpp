fa5d23ffc9e701309c406cbc31bd10d66fdfdb41
Andreas <andreas.l.teigen@gmail.com>
2022-02-13 10:16:15 +0100

    add new sequencer template

---
+++ sequencer/sequencer2.cpp
@@ -0,0 +1,44 @@
+
+
+/*
+Init:
+    Arguments:
+        int Speed mode (1,2,3)
+        int num_cam
+        vector<string> source
+        int fps
+        bool reverse
+        int frame_buffer_size
+*/
+
+// Functionality
+
+    /*
+    Speed Modes: 
+        Fast(1): Quick as possible.
+        Normal(2): Max(Fps, Fast).
+        Manual(3): Manual jump back and fourth.
+    */
+
+    /*
+    Reverse:
+        Run entire sequence in reverse.
+    */
+
+    /*
+    Skip Frames: 
+        X: Skip x amount of frames between every loaded frame, show them at <Speed Modes> speed.
+    */
+
+    /*
+    Multi Camera:
+        Load multiple camera sources at once.
+        Return multiple camera sources at once.
+    */
+
+    /*
+    Multithreading frame buffer:
+        Faster loading of images.
+    */
+
+
